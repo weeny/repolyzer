b6fd08b721620823199bd0c561130287ea222d63
William Wagner <william.wagner@gmail.com>
2018-02-08 16:36:58 -0500

    update to new ui hooks

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+/* I am keeping DOM control in this file. */
 
 
 var main=angular.module("repolyzer",[]);
@@ -10,30 +10,22 @@ main.controller("main",function($scope,$http,Github) {
   var cookies={};
   var GITHUB=Github.CONFIG;
   var GithubRepo=Github.GithubRepo;
+
+  var access_token=null;
+  var modal=angular.element(document.getElementById("authmodal"));
+
+  //Get url parameters
   window.location.search.substr(1).split("&").map(function(param) {
     var kv=param.split("=");
     params[kv[0]]=decodeURIComponent(kv[1])
   });
 
-  var a=document.cookie.split(";");
-  var authorized=false
-  for(var i=0;i<a.length;i++) {
-    var kv=a[i].split('=');
+  //Get cookie values
+  document.cookie.split(";").map(function(param) {
+    var kv=param.split("=");
     cookies[kv[0]]=kv[1];
+  });
 
-  }
-
-
-
-
-
-
-  $scope.auth=function() {
-    // Send user to the github oauth login page
-    window.location="https://github.com/login/oauth/authorize?client_id="+GITHUB.client_id
-  }
-  var access_token=null;
-  var modal=angular.element(document.getElementById("authmodal"));
   if(params["access_token"]) {
     access_token=params["access_token"];
 
@@ -42,6 +34,7 @@ main.controller("main",function($scope,$http,Github) {
 
     // The access token has been consumed, remove it from URL bar
     window.history.pushState({},"Authorized","/repolyzer/");
+
   } else if (cookies["access_token"]!=null) {
     // User prevously logged in
     access_token=cookies["access_token"];
@@ -53,13 +46,22 @@ main.controller("main",function($scope,$http,Github) {
   }
 
 
+  $scope.auth=function() {
+    // Send user to the github oauth login page
+    window.location="https://github.com/login/oauth/authorize?client_id="+GITHUB.client_id
+  }
 
   $scope.load=function load(repo) {
     if(repo!=undefined) $scope.projectname=repo;
     else repo=$scope.projectname;
     var Repo=null;
     if(repo in GITHUB.Repos) {
+      for(var i=0;i<GITHUB.Repos.length;i++) {
+        GITHUB.Repos[i].element.addClass("hidden");
+      }
+
       Repo=GITHUB.Repos[repo];
+      Repo.element.removeClass("hidden");
     } else {
       Repo=GITHUB.Repos[repo]=new GithubRepo(repo,this.access_token,this.URL);
       Repo.element=document.createElement("div");
@@ -81,7 +83,8 @@ main.controller("main",function($scope,$http,Github) {
       Repo.updateFile=function updateFile(file,f) {
         file.size+=f.additions;
         file.size-=f.deletions;
-        file.style.padding=(20+(file.size/20))+"px"
+
+        //file.style.padding=(20+(file.size/20))+"px"
       }
     }
 
